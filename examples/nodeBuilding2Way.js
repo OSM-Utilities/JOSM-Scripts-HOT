a8e4f303b607fc946d9f3eb40b52ebf9220e9cab
bjohas <bjohas+git@gmail.com>
2017-06-09 14:55:08 +0100

    Amended node buildings code; flagged pass-by-ref issues

--- examples/nodeBuilding2Way.js
+++ examples/nodeBuilding2Way.js
@@ -1,19 +1,33 @@
 /*
-  gyslerc
-  Bjoern Hassler, http://bjohas.de
+
+  nodeBuilding2Way.js
+
+  This script (intended for the JOSM scripting plugin) expands node-buildings
+  into way-buildings.
 
   Run like this:
 
   var a= require("JOSM-Scripts-HOT/examples/nodeBuilding2Way.js");
-  a.nodeBuilding2Way(distance on each side of node in metres, 
-        tagKey "building", 
-        tagValue "expanded from nodeBuilding", 
+  a.nodeBuilding2Way(distance the building side,
+        tagKey "some key", 
+        tagValue "some value", 
   );
 
-  example:
+  Example 1:
 
   var a= require("JOSM-Scripts-HOT/examples/nodeBuilding2Way.js");
-  a.nodeBuilding2Way(20, "building", "expanded from nodeBuilding");
+  a.nodeBuilding2Way();
+
+  Example 2:
+
+  var a= require("JOSM-Scripts-HOT/examples/nodeBuilding2Way.js");
+  a.nodeBuilding2Way(5, "some key", "some value");
+
+  Note: The script does not run fast - e.g. 100 buildings will take a few seconds.
+
+  https://github.com/OSM-Utilities/JOSM-Scripts-HOT
+  gyslerc, Bjoern Hassler (http://bjohas.de)
+  June 2017
 
 */
 
@@ -27,14 +41,20 @@
     var geoutils = require("JOSM-Scripts-HOT/lib/geoutils.js");
     const rad = Math.PI/180;
     
-    exports.nodeBuilding2Way = function(distance, key, value){
+    exports.nodeBuilding2Way = function(distance_, key, value){
 	var tagName={}
-	tagName[key]=value;	
+	var distance = 4;
+	if (distance_)
+	    distance = distance_
+	if (key)
+	    if (value)
+		tagName[key]=value; 
 	console.clear();
 	var layer = current_layer(layers); 
 	var buildings = countNodeBuildings(layer); //Find subset of node buildings
-	console.println("Number of node-buildings: " + buildings.numNodeBuildings);
+	console.println("Number of node-buildings to expand: " + buildings.numNodeBuildings);
 	expandNodeBuilding(layer,buildings.nodeBuildings,distance,tagName);
+	console.println("Done");
     };
     
     function countNodeBuildings(layer) {
@@ -50,37 +70,48 @@
 		numNodeBuildings++; 
 	    };
 	};	
-	return{ 
+	return { 
 	    nodeBuildings:nodeBuildings,
 	    numNodeBuildings:numNodeBuildings
 	};
     };	
 
-    function expandNodeBuilding(layer,nodeBuilding,d, tagName) {
-	var dist=d*Math.sqrt(2);
-	var tagDone={};
-	tagDone["building"]="";
-	for(i=0; i<nodeBuilding.length; i++)
+    function expandNodeBuilding(layer, nodeBuilding, d, tagName) {
+	// tagName currently not used. Could be merged into 'tags' in case the user wants to input additional tags
+	// d is the intended length of the building side. We're going to offset diagnonally by half a diagnoal: d*sqrt(2)/2.
+	var dist= d * Math.sqrt(2) / 2;
+	// var tagDone={};
+	for(var i=0; i<nodeBuilding.length; i++)
 	{
+	    var tags = nodeBuilding[i].tags;
+	    // console.println(tags);
 	    var nodes=[];
 	    var lat=nodeBuilding[i].lat*rad;
 	    var lon=nodeBuilding[i].lon*rad;
-	    for(j=0; j<4; j++)
-	    {  
-		var brng=Math.PI/4+j*Math.PI/2;
-     		var offsetpoint=transport(lat,lon,brng,dist);
-		if(j==1)
-		{nodes[j]=nodeBuilding[i];
-		 nodes[j].pos={lat:offsetpoint.lat/rad, lon:offsetpoint.lon/rad};
-		 nodes[j].tags=tagDone;
-		}
-		else
-		{ 	 
+	    for(var j=0; j<4; j++)
+	    {
+		//console.println("j="+tags);
+		var brng = Math.PI/4+j*Math.PI/2;
+     		var offsetpoint = transport(lat,lon, brng, dist);
+		if(j==0){
+		    nodes[j] = nodeBuilding[i];
+		    nodes[j].pos = {lat:offsetpoint.lat/rad, lon:offsetpoint.lon/rad};
+		    // Does not work: If nodes[j].tags is set to null, tags also becomes null.
+		    //nodes[j].tags = null;
+		} else { 	 
 		    nodes[j]=drawNode(offsetpoint.lat/rad,offsetpoint.lon/rad); 
 		}
 	    }
 	    nodes[j+1]=nodes[0];
-  	    drawWays(nodes,tagName,layer);	 
+	    if (tags.source) {
+		tags.source += ";";
+	    } else {
+		tags.source = "";
+	    }
+	    tags.source += "nodeBldgExp";
+	    //console.println("o="+tags);
+  	    drawWays(nodes,tags,layer);
+	    nodes[0].tags = null;
 	}
     };
 
@@ -100,15 +131,16 @@
 	return node;
     };	
     
-    function drawWays(nodeVecIn,tagName,layer){
+    function drawWays(nodeVecIn,tags,layer){
 	// create a way from vector of nodes and add them to layer
-	var w2=wb.withNodes(nodeVecIn).withTags(tagName).create();
+	//console.println(tagName);
+	var w2=wb.withNodes(nodeVecIn).withTags(tags).create();
 	command.add(nodeVecIn).applyTo(layer)
 	command.add(w2).applyTo(layer)
     };	
 
     function current_layer(layers) {
-	console.println("Active layer is " + layers.activeLayer.name)
+	console.println("Active layer: " + layers.activeLayer.name)
 	return layers.activeLayer;
     }	
 }());
